0574b9b1803e5aa4f68312cab26ad5dc2575e102
Qaswar664 <qaswarahmad2019@gmail.com>
2023-02-01 16:09:23 +0500

    mobax in react js with increment and decrement functionality

---
+++ src/store/RootStore.ts
@@ -0,0 +1,15 @@
+import { CountStore } from "./CountStore";
+
+export interface IRootStore {
+    countStore:CountStore
+}
+
+export class RootStore implements IRootStore {
+    countStore:CountStore
+    
+    constructor(){
+        this.countStore=new CountStore(this)
+    }
+
+}
+
